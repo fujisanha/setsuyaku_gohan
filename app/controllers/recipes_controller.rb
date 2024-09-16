2d748acd15851b26c2514e73cb453ec94529506d
EC2 Default User <ec2-user@ip-172-31-5-74.ap-northeast-1.compute.internal>
2024-09-16 09:43:36 +0000

    フォームを部分テンプレートに置き換え

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,12 +3,13 @@ class RecipesController < ApplicationController
     @recipe = Recipe.new
     @details = @recipe.details.build
     @recipe_materials = @recipe.recipe_materials.build
+    @submit_label = "投稿"
   end
   
   def create
     @recipe = Recipe.new(recipe_params)
     if @recipe.save
-      redirect_to recipes_path(@recipe.id)
+      redirect_to recipes_path
     else
       render :new
     end
@@ -26,21 +27,26 @@ class RecipesController < ApplicationController
 
   def edit
     @recipe = Recipe.find(params[:id])
+    @submit_label = "編集"
   end
   
   def update
     @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
-      redirect_to recipe_path(@recipe.id)
+      redirect_to recipe_path(@recipe)
     else
       render :edit
     end
   end
 
   def confirm
+    @recipe = Recipe.find(params[:id])
   end
   
   def destroy
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect_to recipes_path
   end
   
   private
