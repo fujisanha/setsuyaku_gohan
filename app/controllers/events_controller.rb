54e696b51a76ef93102c74c82391d3693aa78b63
EC2 Default User <ec2-user@ip-172-31-5-74.ap-northeast-1.compute.internal>
2024-09-25 04:00:08 +0000

    コントローラーの記述

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,10 +2,46 @@ class EventsController < ApplicationController
   before_action :set_beginning_of_week
   
   def new
+    @events = Event.all
+    @event = Event.new
+  end
+  
+  def create
+    @event = current_user.event(event_params)
+    if @event.save
+      flash[:notice] = "記録しました。"
+      redirect_to event_path
+    else
+      render :new
+    end
+  end
+  
+  def update
+    @event = Event.find(params[:id])
+    if @event.update(event_params)
+      flash[:notice] = "編集しました。"
+      redirect_to event_path
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    @event = Event.find(params[:id])
+    if @event.destroy
+      flash[:notice] = "削除しました。"
+      redirect_to event_path
+    else
+      render :new
+    end
   end
   
   private
   def set_beginning_of_week
     Date.beginning_of_week = :sunday
+  end
+  
+  def event_params
+    params.require(:event).permit(:income, :expense, :start_time)
   end
 end
