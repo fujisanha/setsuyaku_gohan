ab135f9d90dbf1ccec7f12f28784463fe7f162e1
EC2 Default User <ec2-user@ip-172-31-5-74.ap-northeast-1.compute.internal>
2024-10-08 03:28:11 +0000

    admin/comments#indexを作成

--- app/controllers/admin/comments_controller.rb
+++ app/controllers/admin/comments_controller.rb
@@ -3,8 +3,13 @@ class Admin::CommentsController < ApplicationController
   before_action :authenticate_admin!
   
   def index
+    @comments = Comment.all
   end
   
   def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy
+    flash[:notice] = "deleted"
+    redirect_to admin_comments_path
   end
 end
